bff6a4cf7605214a2d2c5a82396c832590dea001
Oleksii Mandrichenko <oleksii.mandrichenko@dewais.com>
2022-05-15 14:50:25 +0300

    ci(test-scope-el): Fix lint pipeline for GitHub IV-33

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ window.addEventListener('load', function load() {
   var mess;
   var webSocket = new WebSocket(gameUrls.list);
 
-  const unusedConstddddJustForCommitTest = 0
+  const unusedConstdddddJustForCommitTest = 0
 
   // Получили команду через WebSocket на начало игры
   function startGameFunc(playerID, gameID) {
