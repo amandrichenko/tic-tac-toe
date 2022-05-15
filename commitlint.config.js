92e099e2369c06cf39bc1dc2b36d779bedd7ce9e
Oleksii Mandrichenko <oleksii.mandrichenko@dewais.com>
2022-05-15 19:57:07 +0300

    fix(ui): Test IV-1

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-    plugins: ['commitlint-plugin-jira-rules'],
-    extends: [
-        // "@commitlint/config-conventional",
-        'jira',
-    ],
-    "rules": {
-        "header-max-length": [2, "always", 72],
-        // "type-enum": [2, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'ci']],
-        // "scope-empty": [2, "never"],
-        "scope-case": [2, "always", 'lower-case'],
-        "scope-enum": [2, "always", ['ui', 'logic']],
-        // "subject-empty": [2, "never"],
-        "subject-case": [2, "always", 'sentence-case'],
-        "subject-min-length": [2, "always", 4],
-        "subject-full-stop": [2, "never", '.'],
-        "body-max-length": [2, "always", 72],
-        // 'references-empty': [2, 'never']
-    },
+  plugins: ['commitlint-plugin-jira-rules'],
+  extends: [
+    '@commitlint/config-conventional',
+    // 'jira',
+  ],
+  rules: {
+    'header-max-length': [2, 'always', 72],
+    // "type-enum": [2, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'ci']],
+    // "scope-empty": [2, "never"],
+    'scope-case': [2, 'always', 'lower-case'],
+    'scope-enum': [2, 'always', ['ui', 'logic']],
+    // "subject-empty": [2, "never"],
+    'subject-case': [2, 'always', 'sentence-case'],
+    'subject-min-length': [2, 'always', 4],
+    'subject-full-stop': [2, 'never', '.'],
+    'body-max-length': [2, 'always', 72],
+    // 'references-empty': [2, 'never']
+  },
 }
 // parserPreset: {
 //     parserOpts: {
