4b73f18e86b6fa9ae05813f08bf8a4ee33e1212d
Oleksii Mandrichenko <oleksii.mandrichenko@dewais.com>
2022-05-15 13:13:11 +0300

    fix(test-scope-el): E

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,11 +4,19 @@ module.exports = {
     ],
     "rules": {
         "header-max-length": [2, "always", 72],
-        "header-case": [2, "always", 'Sentence case'],
-        "header-fill-stop": [2, "always", '.'],
-        "header-min-length": [2, "always", 4],
+        "subject-case": [2, "always", 'sentence-case'],
+        "subject-full-stop": [2, "never", '.'],
+        "header-min-length": [2, "always", 7],
         "scope-empty": [2, "always"],
         "scope-case": [2, "always", 'lower-case'],
         "scope-enum": [2, "always", ['test-scope-el', 'test-scope-another-el']],
+        "body-max-length": [2, "always", 72],
+
     }
 }
+//
+// parserPreset: {
+//     parserOpts: {
+//         issuePrefixes: ['PROJ-']
+//     }
+// },
