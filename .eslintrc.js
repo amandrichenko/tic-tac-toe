c9374ada335688d0e851addcf57ed6aeb18396ad
Oleksii Mandrichenko <oleksii.mandrichenko@dewais.com>
2022-05-15 15:51:33 +0300

    IV-1: Cqecw

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
-  extends: 'eslint:recommended, prettier',
+  extends: ['eslint:recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
