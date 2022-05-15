e60abd97bfa7ebd4382fc04247b807381e20594c
Oleksii Mandrichenko <oleksii.mandrichenko@dewais.com>
2022-05-15 13:10:27 +0300

    fix: Cecedcdwvceverv

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,4 +2,13 @@ module.exports = {
     extends: [
         "@commitlint/config-conventional"
     ],
+    "rules": {
+        "header-max-length": [2, "always", 72],
+        "header-case": [2, "always", 'Sentence case'],
+        "header-fill-stop": [2, "always", '.'],
+        "header-min-length": [2, "always", 4],
+        "scope-empty": [2, "always"],
+        "scope-case": [2, "always", 'lower-case'],
+        "scope-enum": [2, "always", ['test-scope-el', 'test-scope-another-el']],
+    }
 }
